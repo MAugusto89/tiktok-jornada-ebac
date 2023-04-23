d0259b87b3479ccc739e408c3c7a940b261b472c
MAugusto89 <souzadagama966.masdg@gmail.com>
2023-04-23 17:52:46 -0400

    Modificações CSS

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import db from "./config/firebase";
 import { collection, getDocs } from "firebase/firestore/lite";
 
 function App() {
+  
   let maxHeight;
   if(window.innerHeight <= 800){
     maxHeight = window.innerHeight
